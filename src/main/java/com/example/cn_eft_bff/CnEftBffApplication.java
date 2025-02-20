f02e4cf9ca5a7a03b6ff8ee8051c187948570d6e
devOlbap <pa.garridoc@duocuc.cl>
2025-02-20 00:16:41 -0300

    first_commit_bff

---
+++ src/main/java/com/example/cn_eft_bff/CnEftBffApplication.java
@@ -0,0 +1,13 @@
+package com.example.cn_eft_bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnEftBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnEftBffApplication.class, args);
+	}
+
+}
