3a2e44880fb8a41fb1a7557da1b671ce7c79ba9f
devOlbap <pa.garridoc@duocuc.cl>
2025-03-28 14:34:00 -0300

    mods

--- src/main/java/cl/duoc/ejemplo/bff/config/CorsConfig.java
+++ src/main/java/cl/duoc/ejemplo/bff/config/CorsConfig.java
@@ -1,24 +1,24 @@
-package cl.duoc.ejemplo.bff.config;
+// package cl.duoc.ejemplo.bff.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.cors.CorsConfiguration;
+// import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+// import org.springframework.web.filter.CorsFilter;
 
-@Configuration
-public class CorsConfig {
+// @Configuration
+// public class CorsConfig {
 
-    @Bean
-    public CorsFilter corsFilter() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("http://localhost:4200"); // Permite solicitudes desde localhost:4200
-        configuration.addAllowedMethod("*"); // Permite todos los métodos (GET, POST, etc.)
-        configuration.addAllowedHeader("*"); // Permite todos los encabezados
-        configuration.setAllowCredentials(true); // Permite credenciales (opcional)
+//     @Bean
+//     public CorsFilter corsFilter() {
+//         CorsConfiguration configuration = new CorsConfiguration();
+//         configuration.addAllowedOrigin("http://localhost:4200"); // Permite solicitudes desde localhost:4200
+//         configuration.addAllowedMethod("*"); // Permite todos los métodos (GET, POST, etc.)
+//         configuration.addAllowedHeader("*"); // Permite todos los encabezados
+//         configuration.setAllowCredentials(true); // Permite credenciales (opcional)
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return new CorsFilter(source);
-    }
-}
+//         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//         source.registerCorsConfiguration("/**", configuration);
+//         return new CorsFilter(source);
+//     }
+// }
--- src/main/java/cl/duoc/ejemplo/bff/controllers/BffController.java
+++ src/main/java/cl/duoc/ejemplo/bff/controllers/BffController.java
@@ -40,5 +40,8 @@ public class BffController {
 		return ResponseEntity.ok(senalService.read());
 	}
 
+
+	
+
 	
 }
